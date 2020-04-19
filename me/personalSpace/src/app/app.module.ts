23e2bc38ed3f262b988d4991ec3e351bc141f7ca
gciandro13 <gciandrini@gmail.com>
2020-04-19 15:53:16 +0200

    DS_Store

--- me/personalSpace/src/app/app.module.ts
+++ me/personalSpace/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CiandroFirstExpComponentComponent } from './ciandro-first-exp-component/ciandro-first-exp-component.component';
+import { FirstComponentExpPipePipe } from './first-component-exp-pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CiandroFirstExpComponentComponent,
+    FirstComponentExpPipePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
